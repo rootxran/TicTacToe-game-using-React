06d12e4fba13490779d7b11f4f4ff1c61b024d99
Zubair <zubairzahid228@gmail.com>
2023-07-23 17:03:31 +0500

    f-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TicTac from './Components/tictac';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <TicTac></TicTac>
+    </div>
+  );
+}
+
+export default App;
